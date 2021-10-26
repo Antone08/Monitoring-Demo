e069ac74bbfb5a366fe7c173ba93d38a3ba16986
Antonio <amp.olivas00@gmail.com>
2021-10-26 11:16:56 -0600

    updated

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express()
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 
-    const port = process.env.PORT || 4545
 })
+const port = process.env.PORT || 4545
 
 app.listen(port, () => console.log (`Take us to warp ${port}!`))
