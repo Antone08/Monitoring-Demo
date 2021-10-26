9fa528ecb1bc870ae9750e74553d433436d942f8
Antonio <amp.olivas00@gmail.com>
2021-10-26 11:12:26 -0600

    updated index.html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path =require('path')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/indexhtmal'))
+    res.sendFile(path.join(__dirname, '/public/index.html'))
 
     const port = process.env.PORT || 4545
 })
